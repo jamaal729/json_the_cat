beb45c101d1502e98532dca356c146eceda76c79
Jamaal Ahmed <jamaal729@yahoo.com>
2019-10-24 21:23:59 +0000

    Refactored the service.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,30 +1,22 @@
-let input = process.argv;
-let qs = input[2];
-// let qs = input.splice(2, input.length - 2);
-console.log(qs);
-
 const request = require('request');
+const fetchBreedDescription = function(breedName, callback) {
 
-// 'https://api.thecatapi.com/v1/breeds/search?q=Siberian'
-
-request(`https://api.thecatapi.com/v1/breeds/search?q=${qs}`, (error, response, body) => {
-  // console.log('error:', error); // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML for the Google homepage.
-
-  if (error) {
-    return console.error('Error:', error);
-  } else {
-
-    const data = JSON.parse(body);
-    console.log('statusCode:', response && response.statusCode); // Response status code
-
-    if (data.length === 0) {
-      // console.log("not found");
-      console.log(`No cat with name ${qs} found.`);
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      // return console.error('Error:', error);
+      callback(error, null);
     } else {
-      console.log(data.length);
-      console.log(data[0].description);
+      const data = JSON.parse(body);
+      // console.log('statusCode:', response && response.statusCode); // Response status code
+
+      if (data.length === 0) {
+        callback(null, `No cat with name ${breedName} found.`);
+      } else {
+        console.log(data.length);
+        callback(null, data[0].description);
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
