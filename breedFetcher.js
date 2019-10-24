742323963a9cb35a81e53dcf21c1a96ab1f18925
Jamaal Ahmed <jamaal729@yahoo.com>
2019-10-24 20:06:33 +0000

    Fixed error in return values

--- breedFetcher.js
+++ breedFetcher.js
@@ -8,22 +8,23 @@ const request = require('request');
 // 'https://api.thecatapi.com/v1/breeds/search?q=Siberian'
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${qs}`, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  // console.log('error:', error); // Print the error if one occurred
+  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   // console.log('body:', body); // Print the HTML for the Google homepage.
 
-  console.log(typeof body); // Type
-
-  // if (!error) {
-  //   console.log(data);
-  //   const data = JSON.parse(body);
-  //   console.log(`No cat with name ${qs} found.`);
-  // }
-  // console.log(typeof data);
-
-}, function(error, response, body) {
   if (error) {
     return console.error('Error:', error);
+  } else {
+
+    const data = JSON.parse(body);
+    console.log('statusCode:', response && response.statusCode); // Response status code
+
+    if (data.length === 0) {
+      // console.log("not found");
+      console.log(`No cat with name ${qs} found.`);
+    } else {
+      console.log(data.length);
+      console.log(data[0].description);
+    }
   }
-  console.log('Success: ', body);
 });
